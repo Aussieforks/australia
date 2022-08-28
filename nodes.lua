7289bd75b4fbfe1d7123bca16a704901e7acfe40
Maverick2797 <49946595+Maverick2797@users.noreply.github.com>
2022-08-28 23:25:25 +0800

    auStralia, not autralia
    tree fern leaves dropped an unknown item due to a typo in the sapling's name

--- nodes.lua
+++ nodes.lua
@@ -506,7 +506,7 @@ minetest.register_node("australia:tree_fern_leaves", {
 		max_items = 1,
 		items = {
 			{
-				items = {"autralia:sapling_tree_fern"},
+				items = {"australia:sapling_tree_fern"},
 				rarity = 20,
 			},
 			{
