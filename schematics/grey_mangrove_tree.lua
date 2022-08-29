2e35477a221916f5abfb79d05978a45c373d391a
Blockhead <jbis1337@hotmail.com>
2022-08-29 18:52:20 +1000

    Reduce mangrove height back to +0,1 original

--- schematics/grey_mangrove_tree.lua
+++ schematics/grey_mangrove_tree.lua
@@ -1,3 +1,3 @@
 return aus.MangroveTreeSchematic("grey_mangrove_tree",
 	"australia:grey_mangrove_tree", "australia:grey_mangrove_leaves",
-	4, 6)
+	3, 4)
--- schematics/stilted_mangrove_tree.lua
+++ schematics/stilted_mangrove_tree.lua
@@ -1,3 +1,3 @@
 return aus.MangroveTreeSchematic("stilted_mangrove_tree",
 	"australia:stilted_mangrove_tree", "australia:stilted_mangrove_leaves",
-	4, 6)
+	3, 4)
